34f0c6cae3fee5a54a25360cab6d191a10ddb290
Yugeta Koji <yugeta@myntinc.com>
2023-06-13 13:07:16 +0900

    bug-fix : set-css : not-found-error

--- src/set_css.js
+++ src/set_css.js
@@ -6,12 +6,9 @@ export class SetCss{
     if(!script_tag){return}
     const src  = script_tag.getAttribute('src')
     const urlinfo = new Urlinfo(src)
-    const sp   = src.split('/')
-    const path = sp.slice(0,-1).join('/')
     const link = document.createElement('link')
-    const filename = urlinfo.filename.replace('.js' , '')
     link.rel   = 'stylesheet'
-    link.href  = `${urlinfo.origin}/${urlinfo.href}/markdown.css`
+    link.href  = `${urlinfo.origin}/${urlinfo.dir}/markdown.css`
     script_tag.parentNode.insertBefore(link , script_tag)
   }
 
